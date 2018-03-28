9a612152920dce6a66b94e2aa9f03878926cb21c
Mara Averick <maraaverick@gmail.com>
2018-03-28 13:12:33 -0400

    Hacky working version…🙈

--- scripts/tverse_revdeps.R
+++ scripts/tverse_revdeps.R
@@ -3,19 +3,23 @@ library(crandb)
 tverse_crandb <- package("tidyverse")
 
 # function to get maintainer ------------------------------
-get_maintainer <- function(pkg){
-  pkg_crandb <- crandb::package(pkg)
+get_maintainer <- function(pkg) {
+  pkg_crandb <- crandb::package(paste(pkg))
   pkg_crandb[["Maintainer"]]
 }
 
-# get tidyverse reverse depends and imports ----------------
+get_maintainer(lobstr::cst())
+# get tidyverse reverse depends, imports, suggests  ----------------
 packages <- tools::package_dependencies(
   packages = "tidyverse",
-  which = c("Depends", "Imports"), reverse = TRUE
+  which = c("Depends", "Imports", "Suggests"), reverse = TRUE
 )
 
 # package names to character vector ------------------------
-package_list <- c(packages[["tidyverse"]])
+package_vec <- packages[["tidyverse"]]
+
+# package names to list -------------------------------------
+package_list <- as.list(packages[["tidyverse"]])
 
 # unnest packages into tibble -------------------------------
 tverse_revdeps <- tibble::tibble(packages) %>%
@@ -24,4 +28,29 @@ tverse_revdeps <- tibble::tibble(packages) %>%
   mutate(url = str_glue("https://CRAN.R-project.org/package={package_name}"))
 
 # works for one ---------------------------------------------
-get_maintainer(package_list[2])
+get_maintainer(package_vec[2])
+get_maintainer(tverse_revdeps$package_name[1])
+
+package_list[[1]]
+crandb::package(package_list[[1]])
+
+crandb::package(package_vec[1])
+map(package_vec, get_maintainer)
+
+# for loop version --------------------------------------------------------
+df <- data.frame(col = list())
+for (i in 1:length(package_vec)) {
+  col <- get_maintainer(package_vec[i])
+  df[i, 1] <- col # places the first result into row i, column 1
+}
+df <- as.tibble(df)
+
+# maintainers frame -------------------------------------------------------
+maintainers <- df %>%
+  separate(V1, sep = "<", into = c("maintainer", "email")) %>%
+  separate(email, sep = ">", into = c("email", "blah")) %>%
+  select(one_of(c("maintainer", "email"))) %>%
+  add_column(package_name = c(package_vec))
+
+# save -------------------------------------------------------------------
+write_csv(maintainers, here::here("inst", "data", "tverse_revdep_maintainers.csv"))
